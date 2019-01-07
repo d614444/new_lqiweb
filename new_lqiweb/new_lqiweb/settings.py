9f4ed4737b0d7b5157dd4a072d87acd2f24af7e8
ronyayaya <ron@lqi.com.tw>
2019-01-07 21:34:39 +0800

    資料庫確認設計方式與權限提升方式

--- new_lqiweb/new_lqiweb/settings.py
+++ new_lqiweb/new_lqiweb/settings.py
@@ -106,8 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 20
+        
 }
 
 # Internationalization
--- new_lqiweb/webpage/serializers.py
+++ new_lqiweb/webpage/serializers.py
@@ -4,7 +4,8 @@ from rest_framework import serializers
 class Pricetable1Serializer(serializers.ModelSerializer):
 	f22c = serializers.FloatField(source='pricetable7_id.f22c')
 	f04 = serializers.CharField(source='pricetable4_id.f04')
+	f01 = serializers.CharField(source='pricetable4_id.f01.pricetable4_f01')
 
 	class Meta:
 		model = Pricetable1
-		fields = ('id', 'f00', 'f22c','f04')
+		fields = ('id', 'f00', 'f22c','f04','f01')
